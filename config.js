f07ecd4495a9c54cf3bece48d346ae0667df6bef
siasprideofficial <siaspride@gmail.com>
2025-12-18 15:44:16 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,46 @@
+// config.js
+window.AppConfig = {
+  texts: {
+    //----- page 1 -----//
+    title: "Little Suprise!",
+    subtitle: "for you my little baxxha",
+    button1: "Click The Letterbox...",
+    
+    //----- loading page -----//
+    letter_slog: "A Message from My Heart",
+    
+    //----- page 2 -----//
+    letter_title: "Heartly Wishes for you Sista!",
+    
+    letter_bold: "My Lovely Sister.. Doll",
+    letter_content1: "Merry Christmas🎄✨ You're my spacial part of my life. Aapke saath bitaye pal humare liye humesha precious the or rahenge. Wo aapki simplicity aur care jo humara dil ko melt kar diya karti thi...🤍 I Miss them a Lot...",
+    letter_content2: "Iss Christmas bas yahi wish hai ki aap jaha bhi ho hamesha khush raho, safe raho aur life me aage badhte raho🌟. Merry Christmas once again🎅❄️💖",
+    
+    letter_regard: "With endless love and warm wishes,",
+    letter_by: "Your Caring Brother",
+    
+    button2: "Our Memories",
+    
+    //----- page 3 -----//
+    gallery_title: "Gallery♪",
+    gallery_subtitle: "Some memories with my doll",
+    
+    media1_text: "Merry Christmas🎄",
+    media1_cap: "I know we're miles apart💔 but you are always in my heart.🥰",
+    
+    media2_text: "My Therapist👩🏻‍⚕️",
+    media2_cap: "Talking to my doll👸🏻 is my all time therapy🤕 This is what I need the most😭",
+    
+    media3_text: "My Angel👼",
+    media3_cap: "I will never Forget You Doll🫂 coz you came with a 🕯lamp when I was in the dark😨",
+    
+    button3: "Endless Love♥",
+    
+    //----- page 4 -----//
+    card_title: "A Message from your Brother...",
+    card_content: "Tumhari smile hi sabse best gift hai✨ Hamesha strong raho, khud par believe rakho aur apne sapno ke peeche jao🤍 Main hamesha tumhare saath hoon. Merry Christmas 🎄❤️",
+    card_regard: "Keep Smile my Lil. Gem💎",
+    card_by: "★YourBrother"
+    //----- ×•× -----//
+  }
+};
